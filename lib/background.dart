91f4a15099296aa5589c225e672d743597ec9258
Celia-git <celia.hough.programmer@gmail.com>
2024-07-29 14:45:41 -0400

    flutter project

---
+++ lib/background.dart
@@ -0,0 +1,4 @@
+class Background {
+  // Default background image
+  static var image = "abstract";
+}
---
+++ lib/main.dart
@@ -0,0 +1,274 @@
+
+import 'package:flutter/material.dart';
+import 'background.dart' as background;
+
+void main() {
+  runApp(const MyApp());
+}
+
+// Stateless App class
+class MyApp extends StatelessWidget {
+
+  const MyApp({super.key});
+  // Widget as Application Root
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "My New Android App!",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
+      ),
+      home: const HomePage(title: "Home Page"),
+    );
+  }
+}
+
+// Home Page Stateful Widget
+class HomePage extends StatefulWidget {
+  const HomePage({super.key, required this.title});
+  final String title;
+  @override
+  _HomePage createState() => _HomePage();
+}
+
+// Home Page class
+class _HomePage extends State<HomePage> {
+  final String defaultImage = "abstract";
+  String? image = background.Background.image;
+
+  @override
+  @protected
+  @mustCallSuper
+  // Refresh Home page each time dependencies are altered
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _setImage();
+  }
+
+  // Set background image on Home Page
+  void _setImage() {
+    image = background.Background.image;
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: DecoratedBox(
+          // Background Image
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("assets/images/$image.jpg"),
+                  fit: BoxFit.cover),
+            ),
+            // Content
+            child: Column(children: [
+              // Change background button
+              TextButton(
+                  onPressed: () {
+                    background.Background.image = defaultImage!;
+                    didChangeDependencies();
+                  },
+                  style: TextButton.styleFrom(
+                      foregroundColor: Colors.white,
+                      backgroundColor: Colors.deepOrange,
+                      textStyle: const TextStyle(
+                          fontSize: 20, fontWeight: FontWeight.bold)),
+                  child: Text("Reset background to $defaultImage")),
+              // Swipe Controls
+              Expanded(child: GestureDetector(onPanUpdate: (details) {
+                // Swipe right: go back
+                if (details.delta.dx > 10) {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return const FifthPage(title: "Fifth Page");
+                  }));
+                }
+                // Swipe left: go forward
+                else if (details.delta.dx < -10) {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return const SecondPage(title: "Second Page");
+                  }));
+                }
+              }))
+            ])));
+  }
+}
+
+// Second Page Class
+class SecondPage extends StatelessWidget {
+  const SecondPage({super.key, required this.title});
+  final String image = "cove";
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: DecoratedBox(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("assets/images/$image.jpg"),
+                  fit: BoxFit.cover),
+            ),
+            child: Column(children: [
+              ChangeBackgroundButton(image: image),
+              Expanded(child: GestureDetector(onPanUpdate: (details) {
+                // Swipe right: go back
+                if (details.delta.dx > 10) {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return const HomePage(title: "Home Page");
+                  }));
+                }
+                // Swipe left: go forward
+                else if (details.delta.dx < -10) {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return const ThirdPage(title: "Third Page");
+                  }));
+                }
+              }))
+            ])));
+  }
+}
+
+// Third Page Class
+class ThirdPage extends StatelessWidget {
+  const ThirdPage({super.key, required this.title});
+  final String title;
+  final String image = "mountains";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: DecoratedBox(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("assets/images/$image.jpg"),
+                  fit: BoxFit.cover),
+            ),
+            child: Column(children: [
+              ChangeBackgroundButton(image: image),
+              Expanded(child: GestureDetector(onPanUpdate: (details) {
+                // Swipe right: go back
+                if (details.delta.dx > 10) {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return const SecondPage(title: "Second Page");
+                  }));
+                }
+                // Swipe left: go forward
+                else if (details.delta.dx < -10) {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return const FourthPage(title: "Fourth Page");
+                  }));
+                }
+              }))
+            ])));
+  }
+}
+
+// Fourth Page Class
+class FourthPage extends StatelessWidget {
+  const FourthPage({super.key, required this.title});
+  final String title;
+  final String image = "road";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: DecoratedBox(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("assets/images/$image.jpg"),
+                  fit: BoxFit.cover),
+            ),
+            child: Column(children: [
+              ChangeBackgroundButton(image: image),
+              Expanded(child: GestureDetector(onPanUpdate: (details) {
+                // Swipe right: go back
+                if (details.delta.dx > 10) {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return const ThirdPage(title: "Third Page");
+                  }));
+                }
+                // Swipe left: go forward
+                else if (details.delta.dx < -10) {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return const FifthPage(title: "Fifth Page");
+                  }));
+                }
+              }))
+            ])));
+  }
+}
+
+// Fourth Page Class
+class FifthPage extends StatelessWidget {
+  const FifthPage({super.key, required this.title});
+  final String title;
+  final String image = "waves";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: DecoratedBox(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("assets/images/$image.jpg"),
+                  fit: BoxFit.cover),
+            ),
+            child: Column(children: [
+              ChangeBackgroundButton(image: image),
+              Expanded(child: GestureDetector(onPanUpdate: (details) {
+                // Swipe right: go back
+                if (details.delta.dx > 10) {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return const FourthPage(title: "Fourth Page");
+                  }));
+                }
+                // Swipe left: go forward
+                else if (details.delta.dx < -10) {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return const HomePage(title: "Home Page");
+                  }));
+                }
+              }))
+            ])));
+  }
+}
+
+class ChangeBackgroundButton extends StatelessWidget {
+  String image;
+
+  ChangeBackgroundButton({super.key, required this.image});
+
+  @override
+  Widget build(BuildContext context) {
+    // Text Button To Change background.Background.image
+    return TextButton(
+        onPressed: () {
+          background.Background.image = image;
+        },
+        style: TextButton.styleFrom(
+            foregroundColor: Colors.white,
+            backgroundColor: Colors.deepOrange,
+            textStyle:
+            const TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+        child: Text("Set $image as home page background"));
+  }
+}
